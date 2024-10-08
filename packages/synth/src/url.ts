355d1c89a4e1762a766057581022db2d2c14cbe1
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-08 11:46:02 +0200

    Update url.ts

--- packages/synth/src/url.ts
+++ packages/synth/src/url.ts
@@ -2,19 +2,22 @@ import { defaultSound, FIELDS } from './SoundConfig'
 import { Sound } from './types'
 
 export const getSoundFromUrl = (url: URL): Sound => {
-	const fx = { ...defaultSound }
+	const sound = { ...defaultSound }
 	const q = url.searchParams.get('fx')
-	if (!q) return fx
+	if (!q) return sound
 	const urlState = decodeURI(q).split(',')
 	for (let index = 0; index < FIELDS.length; index++) {
 		const name = FIELDS[index]?.name
 		const rawVal = urlState[index]
-		if (name && rawVal && !isNaN(Number(rawVal))) fx[name] = Number(rawVal)
+		if (name && rawVal && !isNaN(Number(rawVal))) sound[name] = Number(rawVal)
 	}
-	return fx
+	return sound
 }
 
-export const getUrlFromSound = (fx: Sound, currentUrl = new URL(location.href)) => {
+export const getUrlFromSound = (
+	fx: Sound,
+	currentUrl = new URL(location.href),
+) => {
 	const params = encodeURI(Object.values(fx).join(','))
 	const searchParams = currentUrl.searchParams
 	searchParams.set('fx', params)
