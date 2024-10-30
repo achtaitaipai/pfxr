9a3738446885a049c216368f32f33b53df4abdb2
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-30 16:38:59 +0100

    itch.io export

--- packages/web/src/lib/ui/Aside.svelte
+++ packages/web/src/lib/ui/Aside.svelte
@@ -8,6 +8,9 @@
 	import Button from './Button.svelte'
 	import History from './History.svelte'
 
+	// clipboard api is blocked on itch.io...
+	const isBundle = import.meta.env.VITE_BUNDLE === 'true'
+
 	const selectTemplate = (template: string) => {
 		if (!(template in TEMPLATES)) return
 		history.add(
@@ -30,7 +33,7 @@
 
 <aside>
 	<h1>
-		<img src="/pfxr/logo.svg" alt="" />
+		<img src="{import.meta.env.BASE_URL}logo.svg" alt="" />
 		Pfxr
 	</h1>
 	<section class="flow">
@@ -44,25 +47,27 @@
 		<History />
 	</section>
 	<section class="actions">
-		<Button onclick={copyLink} full>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				width="1em"
-				height="1em"
+		{#if !isBundle}
+			<Button onclick={copyLink} full>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					width="1em"
+					height="1em"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M15.988 3.012A2.25 2.25 0 0 1 18 5.25v6.5A2.25 2.25 0 0 1 15.75 14H13.5v-3.379a3 3 0 0 0-.879-2.121l-3.12-3.121a3 3 0 0 0-1.402-.791 2.252 2.252 0 0 1 1.913-1.576A2.25 2.25 0 0 1 12.25 1h1.5a2.25 2.25 0 0 1 2.238 2.012ZM11.5 3.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 .75.75v.25h-3v-.25Z"
+						clip-rule="evenodd"
+					/>
+					<path
+						d="M3.5 6A1.5 1.5 0 0 0 2 7.5v9A1.5 1.5 0 0 0 3.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L8.44 6.439A1.5 1.5 0 0 0 7.378 6H3.5Z"
+					/>
+				</svg>
+				Copy link</Button
 			>
-				<path
-					fill-rule="evenodd"
-					d="M15.988 3.012A2.25 2.25 0 0 1 18 5.25v6.5A2.25 2.25 0 0 1 15.75 14H13.5v-3.379a3 3 0 0 0-.879-2.121l-3.12-3.121a3 3 0 0 0-1.402-.791 2.252 2.252 0 0 1 1.913-1.576A2.25 2.25 0 0 1 12.25 1h1.5a2.25 2.25 0 0 1 2.238 2.012ZM11.5 3.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 .75.75v.25h-3v-.25Z"
-					clip-rule="evenodd"
-				/>
-				<path
-					d="M3.5 6A1.5 1.5 0 0 0 2 7.5v9A1.5 1.5 0 0 0 3.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L8.44 6.439A1.5 1.5 0 0 0 7.378 6H3.5Z"
-				/>
-			</svg>
-			Copy link</Button
-		>
+		{/if}
 		<Button onclick={() => downloadSound(history.currentSound)} full>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+//@ts-ignore
+const isBundle = process.env.VITE_BUNDLE === 'true'
+
+console.log(isBundle)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/pfxr/',
+	base: isBundle ? './' : '/pfxr/',
 	plugins: [svelte()],
 })
